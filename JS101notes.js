8e81b621bcdd3574ca3bc4f1ebecf526b97615b0
kareem-azem10 <karem.azem.10@gmail.com>
2025-01-10 01:55:35 +0200

    update files

---
+++ JS101notes.js
@@ -0,0 +1,23 @@
+/*
+nots:
+
+number=double like 1.5 and int (intajer) 
+var x=10.5;
+x = parseint(x);//10
+(datalose)
+
+
+if/else
+if(5>6){
+console.log("batata")
+else
+if(....){
+example for if
+}
+}//else
+
+addtional nots:
++= addition assignment
+>= Greater Than or Equal to
+=======
+*/
